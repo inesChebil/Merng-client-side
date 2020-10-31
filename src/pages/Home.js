fb6ece32c191f2f50e96ed06a6bb054fb4308bcc
ines Chebil <ines.chebil26@gmail.com>
2020-10-31 12:20:16 +0100

    Displaying Posts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,56 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
 
-function Home() {
+import PostCard from "../components/PostCard";
+const Home = () => {
+  //   give getPosts an alias "Posts"
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  //   if (posts) {
+  //     console.log(posts);
+  //   }
+  //   const posts = data.getPosts;
+
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading Posts ...</h1>
+        ) : //  if there is posts
+        data.getPosts ? (
+          data.getPosts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        ) : null}
+      </Grid.Row>
+    </Grid>
   );
-}
+};
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 export default Home;
